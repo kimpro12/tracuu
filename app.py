0eea94f238c995c4ee25aa2be3dd40f7bbf6d857
kimbro <95573884+kimpro12@users.noreply.github.com>
2025-07-19 23:38:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -104,5 +104,7 @@ def rank():
         'equivalents': equivalents
     })
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
